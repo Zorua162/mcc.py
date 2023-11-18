da61fac45eb1e8a54ceddd623f11e4c24e4db225
zorua162 <jakeirwin03@gmail.com>
2023-11-18 15:10:40 +0000

    Updated commands generation

--- mcc/commands/GetPlayersLatencyCommand.py
+++ mcc/commands/GetPlayersLatencyCommand.py
@@ -1,4 +1,4 @@
-from command import Command
+from mcc.command import Command
 
 
 class GetPlayersLatencyCommand(Command):
--- mcc/commands/SneakCommand.py
+++ mcc/commands/SneakCommand.py
@@ -1,4 +1,4 @@
-from command import Command
+from mcc.command import Command
 
 
 class SneakCommand(Command):
