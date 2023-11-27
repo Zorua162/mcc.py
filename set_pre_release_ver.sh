3a540d0d894650710dce91721d1142d67be710df
zorua162 <55893520+Zorua162@users.noreply.github.com>
2023-11-27 22:14:59 +0000

    Add step to pipeline which adds a pre version to testpypi, which gets incremented with each release

---
+++ set_pre_release_ver.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+set -x
+latest_package=$(curl https://test.pypi.org/simple/mcc-py/ \
+                | sed 's/<[^>]*>//g' | tail -n 3 | head -n 1)
+latest_ver=$(echo "$latest_package" | sed 's/mcc.py-//g' | sed 's/.tar.gz//g')
+# pre_ver=$(echo "$latest_ver" | sed -E 's/[0-9]+.[0-9]+.[0-9]+//g')
+pre_ver=-pre1
+if [[ "$pre_ver" == "" ]]; then
+    dev_ver="$latest_ver"-pre1
+else
+    prev_pre_ver=${pre_ver/-pre/}
+    next_ver=$((prev_pre_ver+1))
+    dev_ver=$latest_ver-pre$next_ver
+fi
+# sed -i "s/version = \"(.*?)\"/version = \"$dev_ver\"/g" pyproject.toml
+sed -i -E 's/version = "(.*?)"/version = "'"$dev_ver"'"/g' pyproject.toml
