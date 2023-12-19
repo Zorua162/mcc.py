d56e9e87c1e23981f963ca9c86b2af706a787711
zorua162 <55893520+Zorua162@users.noreply.github.com>
2023-12-19 12:36:36 +0000

    Added check if publish should happen

---
+++ check_new_ver.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+set -x
+echo "Get latest test package version"
+latest_package=$(curl https://test.pypi.org/simple/mcc-py/ \
+                | sed 's/<[^>]*>//g' | tail -n 3 | head -n 1)
+echo "Extract the version"
+latest_ver=$(echo "$latest_package" | sed 's/mcc.py-//g' | sed 's/.tar.gz//g')
+echo "latest_ver $latest_ver"
+export latest_ver
+# sed -i -E 's/version = "(.*?)"/version = "'"$dev_ver"'"/g' pyproject.toml
+
+pyproject_ver=$(cat pyproject.toml | grep version | grep -Eo '".*"' | cut -d '"' -f 2)
+echo "pyproject_version $pyproject_ver"
+export pyproject_ver
